8cb646d4d31e7d0381f52d23f46e2725f1273dbc
Hanna Shylina <hashy@ephyros.com>
2024-10-09 17:40:11 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Main from "./components/Main.vue";
+</script>
+
+<template>
+  <header>
+      <h1>The weather app</h1>
+  </header>
+
+  <main>
+    <Main />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
