d36e37ccd2b5a9c5eee01076d56a202e51d6ad67
Hanna Shylina <hashy@ephyros.com>
2024-10-12 12:22:58 +0300

    Added logic for favorites display

--- src/components/CityCards.vue
+++ src/components/CityCards.vue
@@ -9,9 +9,28 @@ import SearchFrom from "./SearchFrom.vue";
 
 const citiesStore = useCitiesStore()
 
+const displayCities = computed(() => {
+    let result
+    switch (citiesStore.display){
+        case 'all':
+            result = citiesStore.cities
+            break
+        case 'fav':
+            result = citiesStore.favCities
+            break
+        default:
+            result = citiesStore.favCities
+    }
+    return result
+})
+
 const MAX_CARDS_NUMBER = 5
 const isCardsNumberLessThanMax = computed(() => {
     return citiesStore.cities.length < MAX_CARDS_NUMBER
+})
+
+const isDisplayAddButton = computed(() => {
+    return citiesStore.display === 'all' && isCardsNumberLessThanMax
 })
 
 let isModalOpen = ref(false)
@@ -25,13 +44,13 @@ function closeModal(){
 </script>
 
 <template>
-    <CityCard v-for="(city, index) in citiesStore.cities"
+    <CityCard v-for="(city, index) in displayCities"
               :city="city"
               :index="index"
               :key="city.id"></CityCard>
     <button @click="openModal()"
             class="add-city-button"
-            v-if="isCardsNumberLessThanMax">
+            v-if="isDisplayAddButton">
         Add new city
     </button>
     <ModalWindow :open="isModalOpen"
