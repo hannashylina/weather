5e006bf7cd6fbeefbc31767c3a1537c21d7e9977
Hanna Shylina <hashy@ephyros.com>
2024-10-23 21:36:58 +0300

    Added message in case user denies to see location

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, computed, watch } from 'vue'
+import { onMounted, computed, ref } from 'vue'
 import { useCitiesStore } from './stores/cities'
 import CityCards from "./CityCards.vue"
 import DisplayToggle from "./DisplayToggle.vue"
@@ -12,10 +12,13 @@ const API_KEY = import.meta.env.VITE_OPEN_WEATHER_API_KEY
 const citiesStore = useCitiesStore()
 const isCitiesListNotEmpty = computed(() => citiesStore.cities.length > 0)
 const isActiveCity = computed(() => Object.keys(citiesStore.activeCity).length > 0)
+let isGeolocationSuccess = ref(false)
+let displayErrorMessage = ref('')
 
 onMounted(() => {
     if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition((location) => {
+            isGeolocationSuccess.value = true
             const city = {
                 lat: location.coords.latitude,
                 lon: location.coords.longitude
@@ -25,6 +28,14 @@ onMounted(() => {
                     citiesStore.setDefaultCity(res.data)
                     citiesStore.setActiveCity(res.data)
                 })
+        }, (error) => {
+            isGeolocationSuccess.value = false
+            if(error.message.toLowerCase().includes('user denied')){
+                displayErrorMessage.value = 'Please allow website to use your location'
+            }
+            else {
+                displayErrorMessage.value = error.message
+            }
         })
     }
     citiesStore.getFavoriteCities()
@@ -35,9 +46,14 @@ onMounted(() => {
 <template>
     <section class="row">
         <div class="col col-60 col-sm-100">
-            <DisplayToggle></DisplayToggle>
-            <div class="cities-wrapper">
-                <CityCards v-if="isCitiesListNotEmpty"></CityCards>
+            <div v-if="isGeolocationSuccess">
+                <DisplayToggle></DisplayToggle>
+                <div class="cities-wrapper">
+                    <CityCards v-if="isCitiesListNotEmpty"></CityCards>
+                </div>
+            </div>
+            <div v-else>
+                {{ displayErrorMessage }}
             </div>
         </div>
         <div class="col col-40 col-sm-100">
